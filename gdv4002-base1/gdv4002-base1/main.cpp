2705e61fc17b08c05f534666aebf533136d35d7f
Hill <st20324344@outlook.cardiffmet.ac.uk>
2025-11-06 15:43:15 +0000

    player movement

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -1,11 +1,16 @@
 #include "Engine.h"
+#include "Keys.h"
+#include <bitset>
 #include <string.h>
 #include <conio.h>
 // Function prototypes
 void myUpdate(GLFWwindow* window, double tDelta);
+void myKeyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods);
+
+std::bitset<5> keys{ 0x0 };
 
 int main(void) {
-
+	
 	float playerVelocity = 2.0f;
 	float anglesPerSecond = glm::radians(45.0f);
 	// Initialise the engine (create window, setup OpenGL backend)
@@ -40,7 +45,7 @@ int main(void) {
 	
 	
 	setViewplaneWidth(10.0f);
-	
+	setKeyboardHandler(myKeyboardHandler);
 
 
 	// Enter main loop - this handles update and render calls
@@ -57,12 +62,84 @@ int main(void) {
 
 void myUpdate(GLFWwindow* window, double tDelta) {
 
-	float player1RotationSpeed = glm::radians(90.0f);
-	GameObject2D* player1 = getObject("player1");
-	player1->orientation += player1RotationSpeed * tDelta;
+	static float playerSpeed = 1.0f; // distance per second
+	float playerRotationSpeed = glm::radians(90.0f);
+	GameObject2D* player = getObject("player1");
+
+	if (keys.test(Key::W) == true) {
+
+		player->position.y += playerSpeed * (float)tDelta;
+	}
+	
+	if (keys.test(Key::S) == true) {
+
+		player->position.y -= playerSpeed * (float)tDelta;
+	}
+	if (keys.test(Key::A) == true) {
+
+		player->orientation += playerRotationSpeed * tDelta;
+	}
+	if (keys.test(Key::D) == true) {
+
+		player->orientation -= playerRotationSpeed * tDelta;
+	}
 
 	float player2RotationSpeed = glm::radians(90.0f);
 	GameObject2D* player2 = getObject("player2");
 	player2->orientation += player2RotationSpeed * tDelta;
 }
 
+void myKeyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	// Check if the key was just pressed
+	if (action == GLFW_PRESS) {
+
+		// now check which key was pressed...
+		switch (key)
+		{
+		case GLFW_KEY_ESCAPE:
+			
+				glfwSetWindowShouldClose(window, true);
+			break;
+		
+		case GLFW_KEY_W:
+			keys[Key::W] = true;
+			break;
+		case GLFW_KEY_S:
+			keys[Key::S] = true;
+			break;
+
+		case GLFW_KEY_A:
+			keys[Key::A] = true;
+			break;
+		case GLFW_KEY_D:
+			keys[Key::D] = true;
+			break;
+
+		}
+
+	}
+	// If not pressed, check the key has just been released
+	else if (action == GLFW_RELEASE) {
+		switch (key)
+		{
+		
+		
+		case GLFW_KEY_W:
+			keys[Key::W] = false;
+			break;
+		case GLFW_KEY_S:
+			keys[Key::S] = false;
+			break;
+		case GLFW_KEY_A:
+			keys[Key::A] = false;
+			break;
+		case GLFW_KEY_D:
+			keys[Key::D] = false;
+			break;
+
+		}
+
+		// handle key release events
+	}
+}
